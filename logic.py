3bb427da2535e850ed41968e9367d26e018bdc11
aldement <aldement117@gmail.com>
2025-04-24 18:38:55 +0300

    Updated logic

--- logic.py
+++ logic.py
@@ -1,14 +1,13 @@
 import sqlite3
 from telebot import types
 
-# === FAQ данные ===
+# Самые популярные вопросы
 faq_data = {
     "Какие профессии самые востребованные?": "Сейчас в топе IT-специальности, врачи, инженеры и логисты.",
     "Как выбрать профессию по интересам?": "Лучше пройти профориентационный тест или определить свои сильные стороны.",
     "Сколько зарабатывает дизайнер?": "В среднем от 80 до 150 тысяч рублей, в зависимости от специализации и опыта."
 }
 
-# === Клавиатура с FAQ ===
 def get_faq_keyboard():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     markup.add(*faq_data.keys(), "Другое")
@@ -17,7 +16,6 @@ def get_faq_keyboard():
 def get_faq_answer(question_text):
     return faq_data.get(question_text)
 
-# === Инициализация БД ===
 def init_db():
     conn = sqlite3.connect("profession.db")
     cursor = conn.cursor()
@@ -32,7 +30,7 @@ def init_db():
     conn.commit()
     conn.close()
 
-# === Сохранение вопроса пользователя ===
+# Сохранение вопроса пользователя 
 def save_question_to_db(user_id, username, question_text):
     conn = sqlite3.connect("profession.db")
     cursor = conn.cursor()
